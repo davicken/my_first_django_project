3ade134f242d9e094fee37935a3eb1d430292504
davicken <mwesigwadavidk@gmail.com>
2019-04-29 12:27:33 +0300

    adds tests

--- poll_application/polls/admin.py
+++ poll_application/polls/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from .models import Question
 
 # Register your models here.
 from .models import Question
 from .models import Choice
-admin.site.register(Question)
+# admin.site.register(Question)
 
 admin.site.register(Choice)
+
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+#     admin.site.register(Question)
+
+#     from django.contrib import admin
+# from .models import Question
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None,
+    {'fields': ['question_text']}),
+    ('Date information', {'fields': ['pub_date']}),
+    ]
+    admin.site.register(Question)
